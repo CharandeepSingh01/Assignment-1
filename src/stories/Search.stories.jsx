117bc86596fd73bd5ca1b0e786b12990b2fc6332
CharandeepSingh01 <login.charandeep@gmail.com>
2023-08-06 21:11:51 +0530

    changes

--- src/stories/Search.stories.jsx
+++ src/stories/Search.stories.jsx
@@ -1,9 +1,7 @@
-// Search.stories.jsx
-
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import Search from '../component/search/Search'; // Assuming 'Search.js' is the file containing the Search component
-import {text } from '@storybook/addon-knobs';
+import Search from '../component/search/Search';
+
 
 export default {
   title: 'Components/Search',
@@ -11,17 +9,11 @@ export default {
   
 };
 
-// Default story
 export const Default = () => {
-  // Using the 'text' knob to allow dynamic input for the searchText prop
-  const searchText = text('Search Text', '');
-
-  // action() function from Storybook addon-actions allows us to log when onChange event occurs
+ 
   const onChange = (event) => {
     action('Input changed')(event.target.value);
   };
 
-  return <Search searchText={searchText} onChange={onChange} />;
+  return <Search  onChange={onChange} />;
 };
-
-// You can add more stories with different props here, if needed
